f5e853c7fbaa44ed19cc2ffbc87cf84484fa8a43
santhoshkanna021 <santhoshkannar802@gmail.com>
2025-09-09 13:33:35 +0530

    new update the backend URL 1

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const data = { name, email, phone, subject, message };
 
       try {
-        const res = await fetch("https://portfolio-backend-alpha-blue.vercel.app", {
+        const res = await fetch("https://portfolio-backend-alpha-blue.vercel.app/api/contact", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
